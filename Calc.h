8d20b6f63e3258a89deac84519b8c4d0ba45d159
Hatim Binani <33985131+TheHeisenberg@users.noreply.github.com>
2019-03-22 12:44:01 +0100

    initialisation du visiteur (#5)

--- Calc.h
+++ Calc.h
@@ -9,87 +9,51 @@
 #include "antlr4-runtime.h"
 #include "GrammarBaseVisitor.h"
 
-using namespace std;
-
 class Calc: public GrammarBaseVisitor {
 public:
+	Calc();
+	virtual ~Calc();
 
 // PROG
-        antlrcpp::Any visitProg(GrammarParser::ProgContext *ctx) override{
-        //TODO        
-	return 0;
-    }
+        antlrcpp::Any visitProg(GrammarParser::ProgContext *ctx) override;
     
 // FUNC
-        antlrcpp::Any visitFunc(GrammarParser::FuncContext *ctx) override{
-	//TODO        
-	return 0;
-    }
+        antlrcpp::Any visitFunc(GrammarParser::FuncContext *ctx) override;
 
 //BLOCK    
-        antlrcpp::Any visitBlock(GrammarParser::BlockContext *ctx) override{
-        //TODO
-	return 0;
-    }
+        antlrcpp::Any visitBlock(GrammarParser::BlockContext *ctx) override;
     
 // STATEMENT
-        antlrcpp::Any visitReturn(GrammarParser::ReturnContext *ctx) override{
-        //TODO
-	return 0;
-    }
+        antlrcpp::Any visitReturn(GrammarParser::ReturnContext *ctx) override;
 
-	antlrcpp::Any visitDefvariable(GrammarParser::DefvariableContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitDefvariable(GrammarParser::DefvariableContext *ctx) override;
     
 //EXPR
-        antlrcpp::Any visitConst(GrammarParser::ConstContext *ctx) override{
-        //TODO
-	return 0;
-    }
+        antlrcpp::Any visitConst(GrammarParser::ConstContext *ctx) override;
 
-	antlrcpp::Any visitVar(GrammarParser::VarContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitVar(GrammarParser::VarContext *ctx) override;
 
-	antlrcpp::Any visitPlus(GrammarParser::PlusContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitPlus(GrammarParser::PlusContext *ctx) override;
 
-	antlrcpp::Any visitMinus(GrammarParser::MinusContext *ctx) override{
-	//TODO
-	return 0;
-    }
-	antlrcpp::Any visitMult(GrammarParser::MultContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitMinus(GrammarParser::MinusContext *ctx) override;
 
-	antlrcpp::Any visitDiv(GrammarParser::DivContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitMult(GrammarParser::MultContext *ctx) override;
+
+	antlrcpp::Any visitDiv(GrammarParser::DivContext *ctx) override;
 
 //TYPE
-	antlrcpp::Any visitTypeint(GrammarParser::TypeintContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitTypeint(GrammarParser::TypeintContext *ctx) override;
 
 //DECLVAR
-	antlrcpp::Any visitDeclvar(GrammarParser::DeclvarContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitDeclvar(GrammarParser::DeclvarContext *ctx) override;
 
 //OPTINIT
-	antlrcpp::Any visitInitvide(GrammarParser::InitvideContext *ctx) override{
-	//TODO
-	return 0;
-    }
+	antlrcpp::Any visitInitvide(GrammarParser::InitvideContext *ctx) override;
+
+private :
+	std::ofstream output;
+	std::unordered_map<std::string, int> table;
+	int offset;
 	
 };
 
